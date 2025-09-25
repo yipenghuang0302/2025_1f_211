1a17a96a9400ff5f32b981d7f0f66feccbefd0e6
Yipeng Huang <yh804@ilab2.cs.rutgers.edu>
2025-09-25 12:48:23 -0400

    Adding BST example of using a queue.

---
+++ pa2/bstLevelOrder/bst.h
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// BINARY SEARCH TREE
+
+typedef struct BSTNode BSTNode;
+struct BSTNode {
+    int key;
+    struct BSTNode* l_child; // nodes with smaller key will be in left subtree
+    struct BSTNode* r_child; // nodes with larger key will be in right subtree
+};
+
+// Add new data to the BST using recursion
+struct BSTNode* insert ( struct BSTNode* root, int key ) {
+
+    // Base case: If the BSTNode here doesn't yet exist
+    if (root==NULL) {
+        root = malloc(sizeof(BSTNode));
+        root->l_child = NULL;
+        root->r_child = NULL;
+        root->key = key;
+    }
+
+    // If the BSTNode already exists, then insert key in correct subtree
+    if ( key<root->key ) {
+        root->l_child = insert( root->l_child, key );
+    } else if ( key==root->key ) {
+        // duplicates are ignored
+    } else { // key>root->key
+        root->r_child = insert( root->r_child, key );
+    }
+    return root;
+}
+
+// Delete the BST using recursion and postorder traversal
+void delete_bst ( BSTNode* root ) {
+    if ( root->r_child!=NULL ) {
+        delete_bst (root->r_child);
+    }
+    if ( root->l_child!=NULL ) {
+        delete_bst (root->l_child);
+    }
+    free (root);
+}
+
